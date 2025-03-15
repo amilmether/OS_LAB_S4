54041b4ce2b652a962da7671811ec3c657e0c410
Amil <amilmether37@gmail.com>
2025-03-15 16:24:15 +0530

    added origin open system call files

--- IOSystemCalls/open3.c
+++ IOSystemCalls/open3.c
@@ -4,11 +4,12 @@
 #include<string.h>
 #include<unistd.h>
 int main(){
-    printf("Program Running...");
     int fd;
     char wbuf[128]="Hello";
+    char rbuf[128];
     fd=open("file.txt",O_RDWR);//edit the file.txt and add Hello to it
-    write(fd,wbuf,strlen(wbuf));
+    read(fd,rbuf,100);//to read 100 bytes of content or it will read how much data if the file do not exist 100 bytes of data
+    printf("%s\n",rbuf);
     close(fd);
     return 0;
 }
